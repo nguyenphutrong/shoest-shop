777ad4cbafc8eed82ea7198d86ae4dde734c2f3a
Nguyễn Phú Trọng <36433757+nptdiary@users.noreply.github.com>
2019-07-02 22:19:48 +0700

    Edit Contact Page

--- resources/views/page/contact.blade.php
+++ resources/views/page/contact.blade.php
@@ -6,24 +6,19 @@
     <div class="container">
         <div class="row d-flex mb-5 contact-info">
             <div class="w-100"></div>
-            <div class="col-md-3 d-flex">
+            <div class="col-md-4 d-flex">
                 <div class="info bg-white p-4">
-                    <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
+                    <p><span class="icon icon-map-marker"></span><span class="text"> {{config('shop.address')}}</span></p>
                 </div>
             </div>
-            <div class="col-md-3 d-flex">
+            <div class="col-md-4 d-flex">
                 <div class="info bg-white p-4">
-                    <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+                    <p><span class="icon icon-phone"></span><a href="tel://{{config('shop.phone')}}"> {{config('shop.phone')}}</a></p>
                 </div>
             </div>
-            <div class="col-md-3 d-flex">
+            <div class="col-md-4 d-flex">
                 <div class="info bg-white p-4">
-                    <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
-                </div>
-            </div>
-            <div class="col-md-3 d-flex">
-                <div class="info bg-white p-4">
-                    <p><span>Website</span> <a href="#">yoursite.com</a></p>
+                    <p><span class="icon icon-envelope"></span><a href="mailto:{{config('shop.email')}}"> {{config('shop.email')}}</a></p>
                 </div>
             </div>
         </div>
@@ -31,16 +26,16 @@
             <div class="col-md-6 order-md-last d-flex">
                 <form action="#" class="bg-white p-5 contact-form">
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Your Name">
+                        <input type="text" class="form-control" placeholder="Tên của bạn">
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Your Email">
+                        <input type="text" class="form-control" placeholder="Địa chỉ email">
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Subject">
+                        <input type="text" class="form-control" placeholder="Chủ đề">
                     </div>
                     <div class="form-group">
-                        <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+                        <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Nội dung"></textarea>
                     </div>
                     <div class="form-group">
                         <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
@@ -48,9 +43,11 @@
                 </form>
 
             </div>
-
-            <div class="col-md-6 d-flex">
-                <div id="map" class="bg-white"></div>
+            <div class="col-md-6 ">         
+                <div class="d-flex align-content-center">
+                <img src="images/contact-us.png" alt="" width="100%">
+                </div>
+       
             </div>
         </div>
     </div>
